9c8c66fd7c6786e44dde9b58badd05c566826a05
devangsuiii <130946505+devangsuiii@users.noreply.github.com>
2024-03-23 15:28:25 +0530

    Add files via upload
    The main page in LearnEase application receives fresh upgrades from the "UI and Usability Enhancements" commit which involves a complete revamp of Learningpage.xaml and Learningpage.xaml.cs.The most important part of these upgrades is introduction of new interface and interaction designs which provide smooth and user-friendly experience. A conversion has been performed on the home page that has changed it from boring appearance to an engaging one by the use of a cutting-edge layout that captures the attention of a student and leads them to learn with keen interest. In this perception the best part of it is that the user experience is better thanks to the increased accessibility and the ease to use the platform as a result, which leads to fewer obstacles the learners have to face. This page features colorful graphics and pictures that tell the story of an excellent app. Improving the user experience by making the app easy to use is also the aim of this page.

---
+++ LearningPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Maui.Controls;
+
+
+namespace LearnEase
+{
+    public partial class LearningPage : ContentPage
+    {
+        public LearningPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnEnrollNowClicked_IntroToProgramming(object sender, EventArgs e)
+        {
+            // Navigate to a new page that shows details for the Introduction to Programming course
+            var courseDetailPage = new CourseDetailPage("Introduction to Programming");
+            await Navigation.PushAsync(courseDetailPage);
+        }
+
+        private async void OnEnrollNowClicked_AdvancedWebDev(object sender, EventArgs e)
+        {
+            // Navigate to a new page that shows details for the Advanced Web Development course
+            var courseDetailPage = new CourseDetailPage("Advanced Web Development");
+            await Navigation.PushAsync(courseDetailPage);
+        }
+
+        private async void OnEnrollNowClicked_DigitalMarketing(object sender, EventArgs e)
+        {
+            // Navigate to a new page that shows details for the Introduction to Programming course
+            var courseDetailPage = new CourseDetailPage("Introduction to Programming");
+            await Navigation.PushAsync(courseDetailPage);
+        }
+
+        private async void OnEnrollNowClicked_googleAds(object sender, EventArgs e)
+        {
+            // Navigate to a new page that shows details for the Advanced Web Development course
+            var courseDetailPage = new CourseDetailPage("Advanced Web Development");
+            await Navigation.PushAsync(courseDetailPage);
+        }
+        private async void OnMoreClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("More Options", "This is the overflow menu", "OK");
+        }
+    }
+}

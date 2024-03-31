5ecf81e725ac68a7df643066a66d23bfe8dab9c8
devangsuiii <130946505+devangsuiii@users.noreply.github.com>
2024-03-31 22:14:27 +0530

    Add files via upload
    Implemented VideoPlayerPage for playing YouTube videos. Added functionality to dynamically load videos based on their ID, enhancing the user's learning experience with multimedia resources.

---
+++ LearnEase/VideoPlayerPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace LearnEase
+{
+    public partial class VideoPlayerPage : ContentPage
+    {
+        public VideoPlayerPage(string videoId)
+        {
+            InitializeComponent();
+
+            // Load YouTube video with the given VideoId
+            LoadYouTubeVideo(videoId);
+        }
+
+        private void LoadYouTubeVideo(string videoId)
+        {
+            // Construct the embed URL for the YouTube video
+            string embedUrl = $"https://www.youtube.com/embed/{videoId}";
+
+            // Load the YouTube video in the WebView control
+            webView.Source = new UrlWebViewSource { Url = embedUrl };
+        }
+    }
+}

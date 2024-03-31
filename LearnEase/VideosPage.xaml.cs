f2a72150333109ec85c0d82b1f36f34466dde55e
devangsuiii <130946505+devangsuiii@users.noreply.github.com>
2024-03-31 22:19:56 +0530

    Add files via upload
    Optimized video playback in VideoPlayerPage by refining the YouTube video loading mechanism. Ensured smoother video experiences and addressed potential performance issues.

---
+++ LearnEase/VideosPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace LearnEase
+{
+    public partial class VideosPage : ContentPage
+    {
+        public VideosPage()
+        {
+            InitializeComponent();
+            BindingContext = new VideosPageViewModel();
+        }
+    }
+   
+}
